2ffbd261e4f81bce7a26464bcd5a254d4bebef6d
Prince Sharma <Prince-919>
2024-08-11 19:23:23 +0530

    complete ratail-store backend

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const itemsRoute = require("../items/itemsRoute");
 const authRoute = require("../auth/userRoute");
 const billsRoute = require("../bills/billRoute");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [process.env.PORTFOLIO_URL],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+  })
+);
 app.use(express.json());
 
 app.use("/api/items", itemsRoute);
--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 const _config = {
   port: process.env.PORT,
   databaseUrl: process.env.MONGO_CONNECTION_STRING,
-  nodeEnv: process.env.NODE_ENV,
+  frontendDomain: process.env.FRONTEND_DOMAIN,
 };
 
 module.exports = Object.freeze(_config);
